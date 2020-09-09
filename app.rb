73a5242492e9218e96c1271473469b88d05a3290
Austin Cummings <aac729@live.com>
2020-09-09 17:58:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Food Forum"
+    "Food Form"
   end 
 
   get '/food_form' do
+    "Food Form"
     erb :food_form
   end
 
